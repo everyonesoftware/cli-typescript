7ff2b3359be803435d14920bef565c2ce35fd11f
Dan Schulte <danschultewhpc@gmail.com>
2026-09-16 12:16:12 -0700

    The CLI is functional at this point. It doesn't do much, but it does actually run

--- sources/main.ts
+++ sources/main.ts
@@ -1,20 +1,43 @@
-import { CharacterWriteStream, CommandLineParameters, CurrentProcess, Iterable } from "@everyonesoftware/common";
+import { CharacterWriteStream, CommandLineCommand, CurrentProcess, join, CommandLineParameter } from "@everyonesoftware/common";
+
+function addEchoCommand(parent: CommandLineCommand, output: CharacterWriteStream): void
+{
+    const echoCommand: CommandLineCommand = parent.addCommand({
+        name: "echo",
+        description: "A command that will echo the command line arguments.",
+    });
+    const reverse: CommandLineParameter = echoCommand.addParameter({
+        name: "reverse",
+        description: "Reverse the arguments.",
+        defaultValue: {
+            notFound: "false",
+            valueNotFound: "true",
+        },
+    });
+    echoCommand.setAction(async () =>
+    {
+        const commandArgs: string[] = parent.getArguments().skip(1).toArray().await();
+        if (reverse.getBooleanValue().await())
+        {
+            commandArgs.reverse();
+        }
+        await output.writeLine(join(" ", commandArgs));
+    });
+}
 
 CurrentProcess.run(async (process: CurrentProcess) =>
 {
     const output: CharacterWriteStream = process.getOutputWriteStream();
-    await output.writeLine("Hello World!");
 
-    const args: Iterable<string> = process.getArguments();
-    await output.writeLine(`Arguments (${await args.getCount()}): ${args.toString()}`);
+    const command: CommandLineCommand = CommandLineCommand.create({
+        name: "everyone",
+        description: "An extensible command line application for the everyone software ecosystem.",
+        aliases: ["e1"],
+        arguments: process.getArguments().skip(2),
+        writeStream: output,
+    });
 
-    const parameters: CommandLineParameters = process.getParameters();
-    parameters.add("apple")
-        .addAlias("a")
-        .setDescription("A parameter named 'apple'.");
-    parameters.add("banana")
-        .addAliases(["b", "bna"])
-        .setDescription("A parameter named 'b'.");
+    addEchoCommand(command, output);
 
-
+    return await command.run();
 });
